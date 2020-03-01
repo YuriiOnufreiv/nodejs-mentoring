733d6c128e2baaa7f8b605f8c0674e1270748bf3
Yurii Onufreiv <yurii_onufreiv@epam.com>
2020-03-01 13:43:18 +0200

    [2.1, 2.2] after review chages
    - add UserService and UserController
    - use user id for removing

--- module-2-rest-with-validation/src/users.controller.js
+++ module-2-rest-with-validation/src/users.controller.js
@@ -1,65 +1,69 @@
-const users = require('./users.service');
-
-exports.processIdParam = (req, res, next, id) => {
-    req.user = users.find(id);
-    if (req.user === undefined || req.user.isDeleted === true) {
-        const error = new Error(`User with id ${id} not found`);
-        error.statusCode = 404;
-        throw error;
+module.exports = class UserController {
+    constructor(userService) {
+        this.userService = userService;
     }
-    next();
-};
 
-exports.findUser = (req, res) => {
-    res.status(200).json(req.user);
-};
-
-exports.findSuggested = (req, res) => {
-    const loginSubstring = req.query.loginSubstring;
-    const limit = req.query.limit;
-    res.status(200).json(users.findSuggested(loginSubstring, limit));
-};
-
-exports.createUser = (req, res) => {
-    const addedUser = users.add(req.body);
-    res.status(201).send(addedUser);
-};
-
-exports.removeUser = (req, res) => {
-    users.delete(req.user);
-    res.status(200).send();
-};
-
-exports.updateUser = (req, res) => {
-    users.update(req.user, req.body);
-    res.status(200).send();
-};
-
-exports.processError = (error, req, res, next) => {
-    const { statusCode, message } = error;
-    res.status(statusCode).json({ message });
-    next(error);
-};
-
-exports.validateSchema = (schema)  => (req, res, next) => {
-    const { error } = schema.validate(req.body, {
-        abortEarly: false,
-        allowUnknown: false
-    });
-    if (error) {
-        res.status(400).json(errorResponse(error.details));
-    } else {
-        return next();
-    }
-};
-
-function errorResponse(schemaErrors) {
-    const errors = schemaErrors.map((error) => {
-        const { path, message } = error;
-        return { path, message };
-    });
-    return {
-        status: 'failed',
-        errors
+    processIdParam = (req, res, next, id) => {
+        req.user = this.userService.find(id);
+        if (req.user === undefined || req.user.isDeleted === true) {
+            const error = new Error(`User with id ${id} not found`);
+            error.statusCode = 404;
+            throw error;
+        }
+        next();
     };
-}
+
+    findUser = (req, res) => {
+        res.status(200).json(req.user);
+    };
+
+    findSuggested = (req, res) => {
+        const loginSubstring = req.query.loginSubstring;
+        const limit = req.query.limit;
+        res.status(200).json(this.userService.findSuggested(loginSubstring, limit));
+    };
+
+    createUser = (req, res) => {
+        const addedUser = this.userService.add(req.body);
+        res.status(201).send(addedUser);
+    };
+
+    removeUser = (req, res) => {
+        this.userService.delete(req.user.id);
+        res.status(200).send();
+    };
+
+    updateUser = (req, res) => {
+        this.userService.update(req.user, req.body);
+        res.status(200).send();
+    };
+
+    processError = (error, req, res, next) => {
+        const { statusCode, message } = error;
+        res.status(statusCode).json({ message });
+        next(error);
+    };
+
+    validateSchema = (schema)  => (req, res, next) => {
+        const { error } = schema.validate(req.body, {
+            abortEarly: false,
+            allowUnknown: false
+        });
+        if (error) {
+            res.status(400).json(this.errorResponse(error.details));
+        } else {
+            return next();
+        }
+    };
+
+    errorResponse = (schemaErrors) => {
+        const errors = schemaErrors.map((error) => {
+            const { path, message } = error;
+            return { path, message };
+        });
+        return {
+            status: 'failed',
+            errors
+        };
+    }
+};
