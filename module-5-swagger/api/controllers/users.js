e0270046485540dd3a43482c4d0fdf14b8aeb28a
Yurii Onufreiv <yurii_onufreiv@epam.com>
2020-03-29 14:44:48 +0300

    [swagger] add route definitions and implementations

---
+++ module-5-swagger/api/controllers/users.js
@@ -0,0 +1,38 @@
+var usersService = require('../services/users.service');
+
+module.exports = {
+    addUser, getSuggestedUsers, getUserById, updateUser, deleteUser
+};
+
+function addUser(req, res) {
+    var user = req.swagger.params.body.value;
+
+    usersService.addUser(user, (addedUser) => res.json(addedUser));
+}
+
+function getSuggestedUsers(req, res) {
+    var loginSubstring = req.swagger.params.loginSubstring.value;
+    var limit = req.swagger.params.limit.value;
+
+    usersService.getSuggestedUsers(loginSubstring, limit, (users) => res.json(users));
+}
+
+function getUserById(req, res) {
+    var userId = req.swagger.params.userId.value;
+
+    usersService.getUserById(userId, (users) => res.json(users),
+        () => res.status(404).json({message: `User ${userId} not found`}));
+}
+
+function updateUser(req, res) {
+    var userId = req.swagger.params.userId.value;
+    var updatedUser = req.swagger.params.body.value;
+
+    usersService.updateUser(userId, updatedUser, (user) => res.json(user));
+}
+
+function deleteUser(req, res) {
+    var userId = req.swagger.params.userId.value;
+
+    usersService.updateUser(userId, () => res.status(200));
+}
