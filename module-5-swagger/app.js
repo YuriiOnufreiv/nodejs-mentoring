b6eee56832819a45c9b54be1ab9f24d70da055ed
Yurii Onufreiv <yurii_onufreiv@epam.com>
2020-03-29 13:50:35 +0300

    [swagger] clean up generated code

--- module-5-swagger/app.js
+++ module-5-swagger/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
@@ -16,8 +14,4 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
 
   var port = process.env.PORT || 10010;
   app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
 });
